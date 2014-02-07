bf3eef3b42c75720c1dbdf3fd469e86016d7148a
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-02-07 13:56:31 -0800

    vendiapp first commit

---
+++ vendiwebapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vendiwebapp::Application.config.secret_key_base = 'd405ddf2785c4e95ae54504bb8e69407b7ee51d6db11551aefcefa21005bd0e18d6a817b75f446b9dc683b12375f3d8cf399824940adfce26acec6f66914a77a'
